dea230c1b5fb352638094a312d628b2a63f0ac81
gustavo <gustavo.isaac@gmail.com>
2024-12-09 15:15:49 +0000

    PacmanGame

---
+++ src/main/java/com/artemissoftware/spring_tutorial/PacmanGame.java
@@ -0,0 +1,23 @@
+package com.artemissoftware.spring_tutorial;
+
+public class PacmanGame implements GamingConsole{
+    @Override
+    public void up() {
+        System.out.println("walk and eat up");
+    }
+
+    @Override
+    public void down() {
+        System.out.println("walk and eat down");
+    }
+
+    @Override
+    public void left() {
+        System.out.println("walk and eat left");
+    }
+
+    @Override
+    public void right() {
+        System.out.println("walk and eat right");
+    }
+}
